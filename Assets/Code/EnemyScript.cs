6e1675ff75529eaca837a6107a4b953fd98e18e9
Goldsolace9995 <rodrigomicopliego@gmail.com>
2023-05-04 19:17:26 +0200

    enemy movement finished

--- Assets/Code/EnemyScript.cs
+++ Assets/Code/EnemyScript.cs
@@ -4,19 +4,17 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
-    public Transform enemy_tr;
     public Transform player_tr;
     public float speed_ = 6.0f;
-    private Vector3 distance_;
 
     // Start is called before the first frame update
     void Start(){
-        enemy_tr = GetComponent<Transform>();
-        player_tr = GetComponent<Transform>();
+
     }
 
     // Update is called once per frame
     void Update(){
-        distance_ = enemy_tr.position - player_tr.position;
+        transform.LookAt(player_tr);
+        transform.position = Vector3.MoveTowards(transform.position, player_tr.position, speed_ * Time.deltaTime);
     }
 }

2a5d974d4feb0b32c55ce4bc4e7d2b6afd94e73d
GoldsolacePVG <rodrigomicopliego@gmail.com>
2023-05-16 14:31:05 +0200

    errors fixed

--- Assets/Code/Enemies/EnemyScript.cs
+++ Assets/Code/Enemies/EnemyScript.cs
@@ -9,7 +9,7 @@ public class EnemyScript : MonoBehaviour
 
     // Start is called before the first frame update
     void Start(){
-
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
--- Assets/Code/Player/Character_script.cs
+++ Assets/Code/Player/Character_script.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Character_script : MonoBehaviour{
     
     public Transform tr_ = null;
+    public Rigidbody rb_ = null;
     public float speed_ = 10f;
     private Vector3 movDir_;
     public int level = 1;
@@ -12,6 +13,7 @@ public class Character_script : MonoBehaviour{
     // Start is called before the first frame update
     void Start(){
         tr_ = GetComponent<Transform>();
+        rb_ = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -19,12 +21,18 @@ public class Character_script : MonoBehaviour{
         movDir_.z = Input.GetAxis("Vertical");
         movDir_.x = Input.GetAxis("Horizontal");
 
-        tr_.position = tr_.position + (movDir_ * speed_ * Time.deltaTime);
+
+
+        // tr_.position = tr_.position + (movDir_ * speed_ * Time.deltaTime);
         
         movDir_.Normalize();
         if(movDir_.z != 0.0f || movDir_.x != 0.0f){
             tr_.forward = movDir_;
         }
 
+    }
+
+    void FixedUpdate(){
+        rb_.velocity = movDir_ * speed_;
     }
 }

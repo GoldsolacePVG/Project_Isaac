2ec3f2b76179551d9ae37ed74e49669e936fdfaa
Goldsolace9995 <rodrigomicopliego@gmail.com>
2023-05-24 20:48:26 +0200

    game over

--- Assets/Code/Player/PlayerController.cs
+++ Assets/Code/Player/PlayerController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -28,6 +29,7 @@ public class PlayerController : MonoBehaviour
             if(lives_ <= 0){
                 lives_ = 0;
                 Destroy(gameObject);
+                SceneManager.LoadScene(2);
             }
         }
     }
---
+++ Assets/Code/Return_menu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Return_menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ReturnMenu(int sceneValue){
+        SceneManager.LoadScene(sceneValue);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
